ab818d5398dec63ab196d24958d49c59f2ede4e7
Ronald <rjskinnerc@unal.edu.co>
2022-04-26 16:30:23 -0500

    Fix linter errors

--- src/modules/list.js
+++ src/modules/list.js
@@ -2,32 +2,33 @@ const TMP_TASKS = [
   {
     description: 'Morning Session',
     completed: false,
-    index: 1
+    index: 1,
   },
   {
     description: 'Pair Programming',
     completed: false,
-    index: 2
+    index: 2,
   },
   {
     description: 'Buy Coffee for Microverse\'s day',
     completed: true,
-    index: 3
+    index: 3,
   },
   {
     description: 'Pair Programming after Lunch',
     completed: false,
-    index: 4
+    index: 4,
   },
   {
     description: 'Check Linters',
     completed: false,
-    index: 5
-  }
-]
+    index: 5,
+  },
+];
 
 export class List {
   tasks = []
+
   constructor() {
     this.tasks = TMP_TASKS;
     this.container = document.getElementById('form-item');
@@ -38,13 +39,12 @@ export class List {
       const taskItem = document.createElement('li');
       taskItem.innerHTML = `
         <span>
-          ${!task.completed ? `<i class="bi bi-square"></i>` : `<i class="bi bi-check-square"></i>`} ${task.description}
+          ${!task.completed ? '<i class="bi bi-square"></i>' : '<i class="bi bi-check-square"></i>'} ${task.description}
         </span>
         <i class="bi bi-three-dots-vertical"></i>
-      `
+      `;
       this.container.after(taskItem);
-
-    })
+    });
   }
 }
 
