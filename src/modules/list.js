53c93de1c8b74cb795e4161fa40b781110c1b1d9
Ronald <rjskinnerc@unal.edu.co>
2022-04-27 10:23:08 -0500

    Remove hardcoded tasks

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,36 +1,7 @@
-const TMP_TASKS = [
-  {
-    description: 'Morning Session',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Pair Programming',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Buy Coffee for Microverse\'s day',
-    completed: true,
-    index: 3,
-  },
-  {
-    description: 'Pair Programming after Lunch',
-    completed: false,
-    index: 4,
-  },
-  {
-    description: 'Check Linters',
-    completed: false,
-    index: 5,
-  },
-];
-
 export class List {
   tasks = []
 
   constructor() {
-    this.tasks = TMP_TASKS;
     this.container = document.getElementById('form-item');
   }
 
